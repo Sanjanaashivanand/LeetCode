9278b661bed728751ae70c7964887cf33e9a8ce3
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2024-11-19 08:53:31 -0700

    Time: 1 ms (94.43%), Space: 44 MB (96%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,26 +2,27 @@ class Solution {
     private List<List<Integer>> res = new ArrayList<>();
     private int[] nums;
 
-    private void permute(List<Integer> path, int idx){
-        if(idx == nums.length){
+    private void permutation(List<Integer> path, int idx){
+        if(idx==nums.length){
             res.add(path);
             return;
         }
 
-        for(int j=idx; j<nums.length; j++){
-            Collections.swap(path, idx, j);
-            permute(new ArrayList<>(path), idx+1);
-            Collections.swap(path, idx, j);
+        for(int i=idx; i<nums.length; i++){
+            Collections.swap(path, idx, i);
+            permutation(new ArrayList<>(path), idx+1);
+            Collections.swap(path, i, idx);
         }
     }
 
     public List<List<Integer>> permute(int[] nums) {
         this.nums = nums;
+
         List<Integer> path = new ArrayList<>();
         for(int i:nums){
             path.add(i);
         }
-        permute(path,0);
+        permutation(path, 0);
         return res;
     }
 }
