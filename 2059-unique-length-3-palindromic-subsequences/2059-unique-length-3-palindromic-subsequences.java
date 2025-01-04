afee52a59b5dfed5ac28abd6f27f7b4745bc05ef
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-01-04 08:39:26 -0700

    Time: 240 ms (29.06%), Space: 47.4 MB (12.3%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int[] map = new int[26];
+
+        for(char c:s.toCharArray()){
+            map[c-'a']++;
+        }
+
+        int[] L = new int[26];
+        HashSet<Integer> S = new HashSet<>();
+        for(char c: s.toCharArray()){
+            map[c-'a']--;
+            for(int j=0; j<26; j++){
+                if(map[j]>0 && L[j]>0){
+                    S.add(26*(c-'a')+j);
+                }
+            }
+            L[c-'a']++;
+        }
+
+        return S.size();
+    }
+}
