2b2693871211a7e2615b0e376d7f38b88eaa2cec
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-02-05 14:22:16 -0700

    Time: 3 ms (86.47%), Space: 49.3 MB (78.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,11 +1,18 @@
 class Solution {
-    private static final int[][] DIRECTIONS = {
-        {-1, 0}, {1, 0}, {0, -1}, {0, 1}
-    };
+    public void dfs(char[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0'){
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
 
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-
         int res = 0;
 
         for(int i=0; i<grid.length; i++){
@@ -18,19 +25,5 @@ class Solution {
         }
 
         return res;
-    }
-
-    private void dfs(char[][] grid, int x, int y){
-        if(x<0 || x>=grid.length || y<0 || y>=grid[0].length || grid[x][y] == '0'){
-                return;
-        }
-
-        grid[x][y] = '0';
-
-        for(int[] direction : DIRECTIONS){
-            int newX = x + direction[0];
-            int newY = y + direction[1];
-            dfs(grid, newX, newY);
-        }
     }
 }
