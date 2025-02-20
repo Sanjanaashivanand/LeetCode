5e7b6ff45f6f64ca7bf11aa293537238b10459e9
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-02-19 19:13:52 -0700

    Time: 106 ms (34.41%), Space: 46.5 MB (59.96%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,44 @@
+class Solution {
+    private class Pair{
+        String word;
+        int dis;
+
+        Pair(String word, int dis){
+            this.word = word;
+            this.dis = dis;
+        }
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> wordSet= new HashSet<>(wordList);
+
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(beginWord, 1));
+        wordSet.remove(beginWord);
+
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+
+            if(curr.word.equals(endWord)){
+                return curr.dis;
+            }
+            
+            for(int i=0; i<curr.word.length(); i++){
+                StringBuilder sb = new StringBuilder(curr.word);    
+                for(char c='a'; c<='z'; c++){
+                    sb.setCharAt(i, c);
+                    String check = sb.toString();
+                    if(wordSet.contains(check)){
+                        System.out.println(check);
+                        q.offer(new Pair(check, curr.dis+1));
+                        wordSet.remove(check);
+                    }
+                }
+            }
+
+            
+        }
+
+        return 0;
+    }
+}
