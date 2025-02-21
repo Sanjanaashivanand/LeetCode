9b538bc3949a32489a3932abc2c4dc9878beb4ab
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-02-21 14:08:20 -0700

    Time: 17 ms (12.59%), Space: 45.6 MB (78.97%) - LeetHub

---
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int m = maze.length; 
+        int n = maze[0].length;
+
+        int[][] dis = new int[m][n];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dis[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int[][] DIRECTIONS = new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}};
+        q.offer(new int[]{entrance[0], entrance[1]});
+        dis[entrance[0]][entrance[1]] = 0;
+        maze[entrance[0]][entrance[1]] = '+';
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int R = curr[0];
+            int C = curr[1];
+
+            for(int[] dir : DIRECTIONS){
+                int nR = R + dir[0];
+                int nC = C + dir[1];
+
+                if(nR<0 || nR>=m || nC<0 || nC>=n || maze[nR][nC]=='+'){
+                    continue;
+                }
+
+                if(dis[R][C] + 1 < dis[nR][nC]){
+                    dis[nR][nC] = dis[R][C] + 1;
+                    q.offer(new int[]{nR, nC});
+                }
+            }
+        }
+
+        int res = Integer.MAX_VALUE;
+
+        // Check the first and last rows (top and bottom boundaries)
+        for (int i = 0; i < n; i++) {
+            if (dis[0][i] != 0) {
+                res = Math.min(res, dis[0][i]);  // top row
+            }
+            if (dis[m-1][i] != 0) {
+                res = Math.min(res, dis[m-1][i]);  // bottom row
+            }
+        }
+
+        // Check the first and last columns (left and right boundaries)
+        for (int i = 0; i < m; i++) {
+            if (dis[i][0] != 0) {
+                res = Math.min(res, dis[i][0]);  // left column
+            }
+            if (dis[i][n-1] != 0) {
+                res = Math.min(res, dis[i][n-1]);  // right column
+            }
+        }
+
+        // If no valid exit is found, return -1; otherwise, return the shortest distance
+        return res == Integer.MAX_VALUE ? -1 : res;
+
+    }
+}
