9d8d15f0c0f3d845c4d1d9258807b6cd9eecac49
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-02-26 16:14:18 -0700

    Time: 1 ms (99.52%), Space: 56.9 MB (69.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,12 +3,14 @@ class Solution {
         int max = Integer.MIN_VALUE;
         int sum = 0;
 
-        for(int i:nums){
-            if(sum<0){
-                sum=0;
-            }    
-            sum+=i;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            
             max = Math.max(max, sum);
+
+            if(sum<0){
+                sum = 0;
+            }
         }
 
         return max;
