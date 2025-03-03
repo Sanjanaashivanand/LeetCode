ca7140469d2c81daa503a73172add5a487581a1c
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-03-03 12:48:55 -0700

    Time: 17 ms (83.77%), Space: 51.6 MB (17.77%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int dfs(int i, int j, String s, String t, int[][] dp){
-        if(j==t.length()) return 1;
-
-        if(i==s.length()) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = dfs(i+1, j+1, s, t, dp) + dfs(i+1, j, s, t, dp);
-        }
-        else{
-            return dp[i][j] = dfs(i+1, j, s, t, dp);
-        }
-    }
-
     public int numDistinct(String s, String t) {
-        int[][] dp = new int[s.length()+1][t.length()+1];
-        for(int i=0; i<s.length(); i++){
-            for(int j=0; j<t.length(); j++){
-                dp[i][j] = -1;
+        int m = s.length();
+        int n = t.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+        // Base case: An empty string t can be formed from any prefix of s, so set dp[i][0] = 1 for all i
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;  // One way to form an empty t from any prefix of s
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return dfs(0, 0, s, t, dp);
+        
+        return dp[m][n];
     }
 }
