42fd687af17d70cd5b89a64509e50c5bc57552f2
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-03-05 19:09:16 -0700

    Time: 3 ms (68.45%), Space: 41.6 MB (81.61%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        int m = s1.length();
+        int n = s2.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        // Fill the first column of the dp table (matching s1 with s3)
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+
+        // Fill the first row of the dp table (matching s2 with s3)
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        // Fill the rest of the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                           (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        // The final result will be in dp[m][n]
+        return dp[m][n];
+    }
+}
