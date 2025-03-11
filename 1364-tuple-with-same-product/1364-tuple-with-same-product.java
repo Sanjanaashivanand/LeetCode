77871391d9041f7a8f77f75473f7a808c25eca63
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-03-11 11:50:14 -0700

    Time: 208 ms (48.21%), Space: 68.2 MB (83.95%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        HashMap<Integer, Integer> productCount = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int product = nums[i] * nums[j];
+                if (productCount.containsKey(product)) {
+                    count += productCount.get(product) * 8;
+                }
+                productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+            }
+        }
+
+        return count;
+    }
+}
