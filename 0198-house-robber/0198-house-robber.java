0a2ce2842256685c15e9e0c3334f97b84516a7d6
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-03-24 16:51:33 -0700

    Time: 0 ms (100%), Space: 41.4 MB (24.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,32 @@
 class Solution {
+
+    //RECURANCE
+    // private int recurse(int idx, int[] nums){
+    //     if(idx<0) return 0;
+
+    //     if(idx==0) return nums[0];
+
+    //     int pick = nums[idx] + recurse(idx-2, nums);
+    //     int not = 0 + recurse(idx-1, nums);
+
+    //     return Math.max(pick, not);
+    // }
+
+    private int recurse(int idx, int[] nums, int[] dp){
+        if(idx<0) return 0;
+
+        if(idx==0) return nums[0];
+        
+        if(dp[idx]!=-1) return dp[idx];
+        int pick = nums[idx] + recurse(idx-2, nums, dp);
+        int not = 0 + recurse(idx-1, nums, dp);
+
+        return dp[idx] = Math.max(pick, not);
+    }
+
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(dp[0],nums[1]);
-
-        for(int i=2; i<nums.length; i++){
-            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
-        } 
-
-        return dp[nums.length-1];
+        Arrays.fill(dp, -1);
+        return recurse(nums.length-1, nums, dp);
     }
 }
