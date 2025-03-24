206ad5153a1cea13db61373b4299e09f36fcdd80
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-03-24 16:56:37 -0700

    Time: 0 ms (100%), Space: 41.7 MB (8.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -12,6 +12,7 @@ class Solution {
     //     return Math.max(pick, not);
     // }
 
+    //MEMO
     private int recurse(int idx, int[] nums, int[] dp){
         if(idx<0) return 0;
 
@@ -25,8 +26,16 @@ class Solution {
     }
 
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return recurse(nums.length-1, nums, dp);
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+
+        for(int i=2; i<nums.length; i++){
+            dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
+        }
+
+        return dp[nums.length-1];
     }
 }
