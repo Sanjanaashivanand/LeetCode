798f0dc7d5baf51c4263ff83d1e2c4c2adef6790
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-03-31 16:22:25 -0700

    Time: 0 ms (100%), Space: 42.4 MB (37.19%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,34 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    // Helper function to recursively flatten the binary tree
-    public void dfs(TreeNode node) {
-        if (node == null) return;
-        
-        // If the node has a left child, we need to process it
-        if (node.left != null) {
-            // Recursively flatten the left subtree
+    public void dfs(TreeNode node){
+        if(node==null) return;
+
+        if(node.left!=null){
             dfs(node.left);
-
-            // Save the right subtree to a temporary variable
+            
             TreeNode temp = node.right;
-
-            // Move the left subtree to the right
             node.right = node.left;
             node.left = null;
 
-            // Find the end of the new right subtree (which was originally the left subtree)
-            while (node.right != null) {
+            while(node.right!=null){
                 node = node.right;
             }
 
-            // Attach the original right subtree to the end of the new right subtree
             node.right = temp;
         }
 
-        // Recursively flatten the right subtree (if not already processed)
         dfs(node.right);
     }
 
     public void flatten(TreeNode root) {
-        dfs(root); // Call dfs to flatten the tree in-place
+        dfs(root);
     }
 }
