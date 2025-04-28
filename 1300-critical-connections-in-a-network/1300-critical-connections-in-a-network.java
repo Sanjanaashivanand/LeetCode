9196e675598a72a130b93690e47b407fe4b5f745
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-04-28 16:59:42 -0700

    Time: 78 ms (88.77%), Space: 118.1 MB (62.41%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,47 +1,46 @@
 class Solution {
-    private int timer =1;
+    private List<List<Integer>> bridges;
+    private List<List<Integer>> adj;
 
-    private void dfs(int node, int parent,List<List<Integer>> adj,
-    int [] vis, int []time, int []low,List<List<Integer>> bridges ){
+    public void dfs(int node, int parent, boolean[] vis, int[] toi, int[] low, int time){
+        vis[node] = true;
+        toi[node] = time;
+        low[node] = time;
+        time++;
 
-        vis[node]=1;
-        time[node] = timer;
-        low[node]=timer;
-        timer++;
-        
-        for(int it : adj.get(node)){
-            if(it==parent )continue;
-            if(vis[it]==0){
-                dfs(it,node,adj,vis,time,low,bridges);
-                low[node]=Math.min(low[node],low[it]);
-                //can it be a bridge
-                if(low[it]>time[node]){
-                    bridges.add(Arrays.asList(it,node));
+        for(int neigh : adj.get(node)){
+            if(neigh==parent) continue;
+
+            if(!vis[neigh]){
+                dfs(neigh, node, vis, toi, low, time);
+                low[node] = Math.min(low[neigh], low[node]);
+                if(low[neigh]>toi[node]){
+                    bridges.add(Arrays.asList(neigh, node));
                 }
             }
             else{
-                low[node]=Math.min(low[node],low[it]);
+                low[node] = Math.min(low[neigh], low[node]);
             }
         }
     }
+
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        //form a graph from connections 
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+        this.bridges = new ArrayList<>();
+        this.adj = new ArrayList<>();
+
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+
+        for(List<Integer> edge : connections){
+            adj.get(edge.get(0)).add(edge.get(1));
+            adj.get(edge.get(1)).add(edge.get(0));
         }
-        for(List<Integer> ls : connections){
-            int u = ls.get(0);
-            int v = ls.get(1);
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        // we need time of insertion array , minimum time of insertion array and a visited array
-        int [] vis = new int[n];
-        int [] time = new int[n];
-        int [] low = new int[n];
-        List<List<Integer>> bridges = new ArrayList<>();
-        dfs(0,-1,adj,vis,time,low,bridges);
+
+        int[] toi = new int[n];
+        int[] low = new int[n];
+        boolean[] vis = new boolean[n];
+
+        dfs(0, -1, vis, toi, low, 1);
+
         return bridges;
     }
 }
