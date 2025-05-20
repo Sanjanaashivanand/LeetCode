50bade5e71f6615137534e3c1075105a82fba1e5
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-05-20 15:55:16 -0700

    Time: 0 ms (100%), Space: 12.5 MB (13.81%) - LeetHub

---
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isRobotBounded(self, instructions):
+        """
+        :type instructions: str
+        :rtype: bool
+        """
+        pos = [0, 0]
+        turns = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        dir = turns[2]
+        curr = 0
+
+        for i in instructions:
+            if i == 'G':
+                pos[0] += turns[curr][0]
+                pos[1] += turns[curr][1]
+            elif i == 'L':
+                curr = (curr + 3) % 4  # Turn Left
+            else:  # 'R'
+                curr = (curr + 1) % 4  # Turn Right
+        
+        return (pos[0] == 0 and pos[1] == 0) or curr != 0
+        
+
+        
