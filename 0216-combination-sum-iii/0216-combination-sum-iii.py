68f5ffc3d4f995cc040d72a7c21aa5b8a5f23a5d
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-06-19 13:48:10 -0700

    Time: 0 ms (100%), Space: 12.4 MB (86.01%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,24 +1,43 @@
+
 class Solution(object):
+
+    def backtrack(self, nums, curr_sum, start, k, n):
+        if len(nums) == k:
+            if curr_sum == n:
+                self.res.append(list(nums))
+            return
+        
+        if curr_sum > n or len(nums)>k:
+            return
+
+        for i in range(start+1, 10):
+            if curr_sum + i > n:
+                break
+
+            if i not in nums:
+                nums.add(i)
+                curr_sum+=i 
+                self.backtrack(nums, curr_sum, i, k, n)
+                nums.discard(i)
+                curr_sum-=i 
+        
+
     def combinationSum3(self, k, n):
         """
         :type k: int
         :type n: int
         :rtype: List[List[int]]
         """
-        res = []
+        self.res = []
+        for i in range(1,10):
+            nums = set()
+            nums.add(i)
+            self.backtrack(nums, i, i, k, n)
 
-        def backtrack(start, path, curr_sum):
-            if len(path) == k:
-                if curr_sum == n:
-                    res.append(list(path))
-                return
+        return self.res
+        
+
+
             
-            for i in range(start, 10):
-                if curr_sum + i > n:
-                    break
-                path.append(i)
-                backtrack(i + 1, path, curr_sum + i)
-                path.pop()
 
-        backtrack(1, [], 0)
-        return res
+            
