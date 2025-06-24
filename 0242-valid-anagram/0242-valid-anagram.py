bfdfb9c4822e348c681882e8717219602996767f
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-06-24 13:52:34 -0700

    Time: 15 ms (84.63%), Space: 12.7 MB (68.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,4 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        freq_s = {}
+        freq_t = {}
+
+        for i in s:
+            if i not in freq_s:
+                freq_s[i] = 0
+            freq_s[i] += 1
+
+        for i in t:
+            if i not in freq_t:
+                freq_t[i] = 0
+            freq_t[i] += 1
+
+        if len(freq_s)!=len(freq_t):
+            return False
+
+        for i in s:
+            if i not in freq_t:
+                return False
+            if freq_s[i] != freq_t[i]:
+                return False
+
+        return True
