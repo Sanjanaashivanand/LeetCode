be8187bcb30a884199b136d1a5a4ad79d3670f88
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-06-26 11:34:58 -0700

    Time: 0 ms (100%), Space: 12.6 MB (45.35%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -4,6 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        s = s.strip()
-        strs = s.split(" ")
-        return len(strs[-1])
+        j = len(s) - 1
+
+        while j>=0 and s[j] == " ":
+            j-=1
+        
+        i = j
+        while i>=0 and s[i] != " ":
+            i-=1
+
+        return j-i
