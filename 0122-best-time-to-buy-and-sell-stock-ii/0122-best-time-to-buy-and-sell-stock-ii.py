4d9c3797ac200b9cbf0a1834a95f730c9a86c24d
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-06-26 03:06:15 -0700

    Time: 0 ms (100%), Space: 13.6 MB (45.97%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,35 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        n = len(prices)
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i-1] < prices[i]:
+                profit += prices[i] - prices[i-1]
+            
+        return profit
 
-        # def dfs(idx, profit, haveStock):
-        #     if idx == n:
-        #         return profit
-
-        #     #Buy
-        #     buy = sell = 0
-        #     if haveStock == False:
-        #         buy = max(dfs(idx+1, profit-prices[idx], True), dfs(idx+1, profit, False))
-
-        #     #Sell
-        #     else:
-        #         sell = max(dfs(idx+1, profit+prices[idx], False), dfs(idx+1, profit, True))
-
-        #     return max(buy, sell)
-
-        dp = [[-1] * 2] * (n+1)
-        dp[n][0] = 0
-        dp[n][1] = 0
-
-        for i in range(n-1, -1, -1):
-            #Sell
-            profit = max(prices[i]+dp[i+1][1], dp[i+1][0])
-            dp[i][0] = profit
-
-            #Buy
-            profit = max(-prices[i]+dp[i+1][0], dp[i+1][1])
-            dp[i][1] = profit
-
-        return dp[0][1]
-
+        
