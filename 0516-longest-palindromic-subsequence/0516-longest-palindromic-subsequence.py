5bbb2ee719887405a2e1c9297758ff9ee4b5d568
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-06-30 19:45:32 -0700

    Time: 1829 ms (20.04%), Space: 34.7 MB (21.59%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -6,7 +6,8 @@ class Solution(object):
         """
         n = len(s)
         dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
-        
+        for j in range(1, n + 1):
+            dp[0][j] = j
         rev = s[::-1]
 
         for i in range(n-1, -1, -1):
