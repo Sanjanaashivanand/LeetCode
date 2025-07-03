e5317d655c68cbd992c0c0cbbe828116b41a16f6
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-03 04:49:49 -0700

    Time: 190 ms (33.06%), Space: 18.1 MB (29.39%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -6,28 +6,22 @@ class Solution(object):
         :rtype: int
         """
 
-        def choose(bought, idx, dp):
-            if idx == len(prices):
-                return 0
+        n = len(prices)
+        dp = [[-1 for _ in range(n+1)] for _ in range(2)]
 
-            if dp[bought][idx]!=-1:
-                return dp[bought][idx]
+        dp[0][n] = 0
+        dp[1][n] = 0
 
-            if bought==0:
-                # Choice: Buy now OR skip
-                buy_now = -prices[idx] + choose(1, idx + 1, dp) 
-                skip = choose(0, idx + 1, dp)
-                dp[bought][idx] = max(buy_now, skip)
-                return dp[bought][idx]
-            else:
-                # Choice: Sell now OR hold
-                sell_now = prices[idx] - fee + choose(0, idx + 1, dp) 
-                hold = choose(1, idx + 1, dp)
-                dp[bought][idx] = max(sell_now, hold)
-                return dp[bought][idx]
+        for i in range(n-1, -1, -1):
 
-        dp = [[-1 for _ in range(len(prices))] for _ in range(2)]
-        return choose(0, 0, dp)
+            #Buy = 0 -> buy or not buy
+            dp[0][i] = max(dp[1][i+1]-prices[i], dp[0][i+1])
+
+            #Sell
+            dp[1][i] = max(dp[0][i+1]+prices[i]-fee, dp[1][i+1])   
+
+
+        return dp[0][0]
 
             
 
