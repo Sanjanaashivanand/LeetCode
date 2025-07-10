75c3c52b83e300fcc9de947f952f7549165f86c6
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-09 18:35:33 -0700

    Time: 143 ms (14.49%), Space: 19.7 MB (10.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        profit = 0
         minSoFar = prices[0]
+        profit = 0
 
         for i in range(1, len(prices)):
-            profit = max(profit, prices[i]  - minSoFar)
+            profit = max(profit, prices[i]-minSoFar)
             minSoFar = min(minSoFar, prices[i])
 
         return profit
-        
