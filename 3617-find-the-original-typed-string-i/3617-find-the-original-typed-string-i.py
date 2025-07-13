d785fb328c3d8b59da5b1b2425c64f1c87600b49
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-12 17:32:36 -0700

    Time: 12 ms (89.29%), Space: 12.3 MB (98.45%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def possibleStringCount(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        n, ans = len(word), 1
+        for i in range(1, n):
+            if word[i - 1] == word[i]:
+                ans += 1
+        return ans
