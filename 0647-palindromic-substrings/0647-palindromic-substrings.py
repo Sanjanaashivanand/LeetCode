29c538dc4f8e54684f299d25132b357db869b797
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-13 20:00:49 -0700

    Time: 175 ms (48.51%), Space: 12.5 MB (67.98%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -7,10 +7,21 @@ class Solution(object):
         def isPalindrome(sub):
             return sub == sub[::-1]
 
-        count = 0
-        for i in range(0, len(s)):
-            for j in range(i, len(s)):
-                if isPalindrome(s[i:j+1]):
-                    count+=1
+        def expandAroundCenter(l, r):
+            ans = 0
+            while l>=0 and r<len(s):
+                if s[l]!=s[r]:
+                    return ans
 
-        return count
+                l-=1 
+                r+=1
+                ans+=1
+
+            return ans
+        
+        res = 0
+        for i in range(0, len(s)):
+            res+=expandAroundCenter(i, i+1)
+            res+=expandAroundCenter(i, i)
+
+        return res
