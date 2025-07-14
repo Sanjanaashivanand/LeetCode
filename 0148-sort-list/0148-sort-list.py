8ffcdd1d42c372d737a8a52c1306dfa0443c2507
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-13 19:07:10 -0700

    Time: 439 ms (40.43%), Space: 50.6 MB (88.54%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -4,30 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def sortList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if not head or not head.next:
-            return head
-
-        slow, fast = head, head
-        prev = None
-
-        while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
-
-        prev.next = None
-        left = self.sortList(head)
-        right = self.sortList(slow)
-        return self.merge(left, right)       
-
-    def merge(self, list1, list2):
+    def mergeList(self, list1, list2):
         dummy = ListNode(0)
-        temp = dummy
+        temp = dummy 
 
         while list1 and list2:
             if list1.val < list2.val:
@@ -40,10 +19,30 @@ class Solution(object):
 
         if list1:
             temp.next = list1
-
         if list2:
             temp.next = list2
 
         return dummy.next
+
+    def sortList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head or not head.next:
+            return head
+
+        slow = fast = head 
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next 
+
+        prev.next = None
+        left = self.sortList(head)
+        right = self.sortList(slow)
+        return self.mergeList(left, right)
+
 
         
