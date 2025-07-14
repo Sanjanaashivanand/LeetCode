0a8b6ed94e0471d862022cf1be6fa19bc7feb9b7
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-13 21:20:15 -0700

    Time: 701 ms (18.48%), Space: 13.4 MB (10.43%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,16 +5,15 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        goal = len(nums) - 1
+        goal = n-1
         jumps = 0
-
+        
         while goal>0:
-            for i in range(goal):
+            for i in range(0, n):
                 if i + nums[i] >= goal:
-                    goal = i
-                    jumps+=1   
+                    goal = i 
+                    jumps+=1
                     break 
-        
-        return jumps
 
-        
+        return jumps
+            
