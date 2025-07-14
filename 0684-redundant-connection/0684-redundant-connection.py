cdef88c6cf3c123f20036da2ed606d9092b0c0ef
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-14 14:27:58 -0700

    Time: 2 ms (73.77%), Space: 12.7 MB (73.46%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,31 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+
+    def find(self, x):
+        while self.parent[x] != x:
+            x = self.parent[x]
+        return x 
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px == py:
+            return False  # already connected — cycle
+        self.parent[py] = px
+        return True
+
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        n = len(edges) + 1
+        uf = UnionFind(n)
+
+        for fr, to in edges:
+            if not uf.union(fr, to):
+                return [fr, to]
+        
+        return []
