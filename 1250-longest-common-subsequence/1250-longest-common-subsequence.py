8061bc43c83a96aca61ff2e8afc9529472f6b339
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-15 13:48:49 -0700

    Time: 957 ms (24.57%), Space: 34.8 MB (13.6%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -5,16 +5,20 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+
         m = len(text1)
         n = len(text2)
 
         dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
-
+        
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
+                same = 0
                 if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
-                else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+                    same = 1 + dp[i+1][j+1]
+                not_same = max(dp[i+1][j], dp[i][j+1])
+
+                dp[i][j] = max(same, not_same)
 
         return dp[0][0]
+        
