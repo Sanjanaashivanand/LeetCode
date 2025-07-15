38a2cc794a2847473f181f441dd12ce703b64d80
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-15 11:42:29 -0700

    Time: 1383 ms (18.83%), Space: 13.2 MB (28.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,24 +5,34 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        def helper(total, dp):
-            if total == 0:
-                return 0
-            if total < 0:
-                return float('inf')
+        # def helper(total, dp):
+        #     if total == 0:
+        #         return 0
+        #     if total < 0:
+        #         return float('inf')
             
-            if dp[total]!=-1:
-                return dp[total]
+        #     if dp[total]!=-1:
+        #         return dp[total]
 
+        #     mini = float('inf')
+        #     for coin in coins:
+        #         if coin <= total:
+        #             coins_needed = 1 + helper(total - coin, dp)
+        #             mini = min(mini, coins_needed)
+
+        #     dp[total] = mini
+        #     return mini
+            
+        dp = [float('inf') for _ in range(amount+1)]
+        dp[0] = 0
+
+        for total in range(1, amount+1):
             mini = float('inf')
             for coin in coins:
                 if coin <= total:
-                    coins_needed = 1 + helper(total - coin, dp)
+                    coins_needed = 1 + dp[max(0, total-coin)]
                     mini = min(mini, coins_needed)
 
             dp[total] = mini
-            return mini
-            
-        dp = [-1 for _ in range(amount+1)]
-        res = helper(amount, dp)
-        return res if res!=float('inf') else -1
+
+        return dp[amount] if dp[amount]!=float('inf') else -1
