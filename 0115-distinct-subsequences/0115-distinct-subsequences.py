1b97bd453a8619ba7089eca8242fbaea0b08de53
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-15 18:10:59 -0700

    Time: 1112 ms (18.67%), Space: 22.4 MB (80.45%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -8,17 +8,31 @@ class Solution(object):
         m = len(s)
         n = len(t)
 
-        dp = [[0 for _ in range(n+1)]for _ in range(m+1)]
-        
-        for i in range(0, m+1):
-            dp[i][n] = 1
-        
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if s[i] == t[j]:
-                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
-                else:
-                    dp[i][j] = dp[i+1][j]
-        
-        return dp[0][0]
+        if len(s) < len(t):
+            return 0
 
+        def helper(i, j, dp):
+            if i==m:
+                if j==n:
+                    return 1
+                else:
+                    return 0 
+
+            if j==n:
+                return 1
+
+            if dp[i][j]!=-1:
+                return dp[i][j]
+
+            #pick
+            pick = 0
+            if s[i] == t[j]:
+                pick = helper(i+1, j+1, dp) + helper(i+1, j, dp)
+            skip = helper(i+1, j, dp)
+
+            dp[i][j] = max(pick, skip)
+            return max(pick, skip)
+
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        return helper(0,0, dp)
+        
