0b260c2e355d7f7c5d071a77dd6451f2e4b69fd3
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-18 16:47:13 -0700

    Time: 5102 ms (5.02%), Space: 209.5 MB (5.01%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n = len(prices)
+
+        def helper(idx, k, bought):
+            if idx==n or k==0:
+                return 0
+            if (idx, k, bought) in dp:
+                return dp[(idx, k, bought)]
+
+            buy = sell = 0
+            if not bought:
+                buy = max(-prices[idx] + helper(idx+1, k, True), helper(idx+1, k, False))
+            else:
+                sell = max(prices[idx] + helper(idx+1, k-1, False), helper(idx+1, k, True))
+
+            dp[(idx, k, bought)] = max(buy, sell)
+            return max(buy, sell)
+
+        dp = {}
+        return helper(0, 2, False)
+        
