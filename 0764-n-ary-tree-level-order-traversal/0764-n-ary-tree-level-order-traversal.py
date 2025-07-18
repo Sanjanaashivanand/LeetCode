870ecd2a2f86305521444840ae604a8577e67b03
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-17 20:29:55 -0700

    Time: 33 ms (70.56%), Space: 15.5 MB (97.2%) - LeetHub

---
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []        
+
+        result = []
+        previous_layer = [root]
+
+        while previous_layer:
+            current_layer = []
+            result.append([])
+            for node in previous_layer:
+                result[-1].append(node.val)
+                current_layer.extend(node.children)
+            previous_layer = current_layer
+        return result
