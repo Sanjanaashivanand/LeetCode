20234b73ee52c2cca2ba26e9386089d53da0e66c
Sanjana <60909843+Sanjanaashivanand@users.noreply.github.com>
2025-07-19 12:16:44 -0700

    Time: 8367 ms (24.77%), Space: 29.5 MB (7.67%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,40 +1,30 @@
 class Solution(object):
     def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         n = len(nums)
-        memo = {}
+        arr = [0] * (n+2)
+        arr[0] = arr[n+1] = 1
 
-        def helper(picked):
-            if len(picked) == n:
+        for i in range(0, n):
+            arr[i+1] = nums[i]
+
+        def burst(left, right):
+            if left+1 == right:
                 return 0
 
-            key = frozenset(picked)
-            if key in memo:
-                return memo[key]
+            if (left, right) in dp:
+                return dp[(left, right)]
 
-            max_coins = 0
+            maxi = 0
 
-            for idx in range(n):
-                if idx in picked:
-                    continue
+            for i in range(left+1, right):
+                maxi = max(maxi, arr[left] * arr[i] * arr[right] + burst(left, i) + burst(i, right))
+            
+            dp[(left, right)] = maxi
+            return maxi
 
-                l = idx - 1
-                while l >= 0 and l in picked:
-                    l -= 1
-
-                r = idx + 1
-                while r < n and r in picked:
-                    r += 1
-
-                left = nums[l] if l >= 0 else 1
-                right = nums[r] if r < n else 1
-
-                picked.add(idx)
-                coins = left * nums[idx] * right + helper(picked)
-                picked.remove(idx)
-
-                max_coins = max(max_coins, coins)
-
-            memo[key] = max_coins
-            return max_coins
-
-        return helper(set())
+        dp = {}
+        return burst(0, n+1)
